7c13b71a737830b8d89b5810fcc3d9a611af255a
Jose Lopez Vilchez <jose.lopez@escuelaestech.es>
2025-01-20 14:06:31 +0100

    enough to display

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,10 @@
     import { screenDimensions } from '$lib/stores/global';
     import { Footer, FooterIcon } from 'flowbite-svelte';
 	import { GithubSolid, DiscordSolid, TwitterSolid, LinkedinSolid } from 'flowbite-svelte-icons';
+    import ElypseUi from '$lib/components/ElypseUI.svelte';
 	let { children } = $props();
+
+	let proportion = 2/3;
 
 	onMount(() => {
 		
@@ -21,16 +24,54 @@
 
 </script>
 
-{@render children()}
+<div class="backgroundGradient fixed inset-0"> <!-- Background, fixed in place -->
 
-<Footer class="flex justify-center self-center h-max p-2 bg-black">
-	<FooterIcon href="/">
-		<DiscordSolid class="w-8 h-8 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
-	</FooterIcon>
-	<FooterIcon href="/">
-		<GithubSolid class="w-8 h-8 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
-	</FooterIcon>
-	<FooterIcon href="/">
-		<LinkedinSolid class="w-8 h-8 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
-	</FooterIcon>
-</Footer>
+	<!--
+	<div> 
+		<ElypseUi proportion={proportion} identifier="unica">
+			<div class="rounded-full elypseGradient"></div>
+		</ElypseUi>
+	</div>
+	-->
+
+	<div class="h-screen overflow-y-auto"> <!-- The contents can scroll freely thx to this div's classes -->
+
+		{@render children()} <!-- +page.svelte components are rendered here -->
+
+		<Footer class="flex justify-center self-center h-max p-2 bg-black"> <!-- Social media links -->
+			<FooterIcon href="https://discord.com/users/209326831935946752" target="_blank">
+				<DiscordSolid class="w-12 h-12 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
+			</FooterIcon>
+			<FooterIcon href="https://github.com/JoseLopezVilchez" target="_blank">
+				<GithubSolid class="w-12 h-12 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
+			</FooterIcon>
+			<FooterIcon href="https://www.linkedin.com/in/jos%C3%A9-l%C3%B3pez-v%C3%ADlchez-44a0692b3/" target="_blank">
+				<LinkedinSolid class="w-12 h-12 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
+			</FooterIcon>
+		</Footer>
+
+	</div>
+</div>
+
+<style>
+	.backgroundGradient {
+		background: radial-gradient(circle,
+		rgba(14, 0, 14, 1) 0%,
+		rgba(14, 0, 14, 1) 1%, /*Solid dot*/
+		rgb(61, 27, 65) 2%,
+		rgb(77, 32, 82) 3%, /*Ring*/
+		rgb(0, 0, 0) 4%,
+		rgb(42, 27, 65) 5%,
+		rgb(15, 1, 20) 25%, /*Soft fade*/
+		rgb(0, 0, 0) 40%
+		);
+	}
+
+	/*.elypseGradient {
+		background: radial-gradient(circle,
+			rgba(255, 0, 0, 1) 20%,
+			rgba(255, 255, 0, 0.7) 50%,
+			rgba(0, 0, 255, 0) 80%
+		);
+	}*/
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { Img, Tooltip } from "flowbite-svelte";
+    import { Heading, Img, Li, List, Tooltip } from "flowbite-svelte";
 </script>
 
 <div class="w-full grid gap-x-2 lg:gap-x-4 grid-cols-2 md:grid-cols-4 lg:grid-cols-8 xl:grid-cols-12">
@@ -15,14 +15,14 @@
 
         </article>
 
-        <article class="col-span-full w-full mb-4 lg:mb-16 flex flex-col bg-slate-600 rounded-lg p-4 lg:p-8">
+        <article class="col-span-full w-full mb-4 lg:mb-16 flex flex-col bg-purple-600/20 rounded-lg p-4 lg:p-8">
 
             <div class="w-full mb-8 lg:mb-16">
                 <h1 class="text-center font-sans text-4xl lg:text-6xl xl:text-8xl font-bold text-white">Skills</h1>
-                <p class="text-center font-sans text-base lg:text-lg xl:text-xl font-light text-white">Most were taught to me, though there's a few I taught myself. And even if not listed, just from what I already know, it becomes much easier to adapt to something new.</p>
+                <p class="text-center font-sans text-base lg:text-lg xl:text-xl font-light text-white">Most were taught to me, though there's a few I taught myself. After a certain point, you can draw parallels from what you already know to hasten the learning process of new technologies.</p>
             </div>
 
-            <div class="w-full grid gap-2 lg:gap-4 grid-cols-2 lg:grid-cols-4 xl:grid-cols-8 auto-rows-auto bg-slate-600 rounded-lg">
+            <div class="w-full grid gap-2 lg:gap-4 grid-cols-2 lg:grid-cols-4 xl:grid-cols-8 auto-rows-auto bg-violet-950/80 rounded-lg">
 
                 <img src="src/resources/english-logo.png" class="rounded-lg aspect-square p-2" alt=""/>
                 <Tooltip class="p-4">C1 English level, 199 points grade, certified by Cambridge Language Assessment</Tooltip>
@@ -77,16 +77,40 @@
 
         <article class="col-span-full grid gap-2 lg:gap-4 grid-cols-2 lg:grid-cols-4 xl:grid-cols-8 mb-4 lg:mb-16">
 
-            <div class="aspect-[16/9] col-start-1 col-end-3 lg:col-end-4 xl:col-end-7 bg-stone-500">
-
+            <div class="rounded-lg aspect-[16/9] col-start-1 col-end-3 lg:col-end-4 xl:col-end-7 grid gap-2 lg:gap-4 grid-cols-2 p-2 bg-fuchsia-900/20">
+                <div class="col-span-1 p-2">
+                    <Heading tag="h2" class="font-sans text-xl lg:text-2xl xl:text-4xl font-bold text-white pb-4">Technical</Heading>
+                    <List tag="ul" class="space-y-1 font-sans text-base lg:text-lg xl:text-xl font-light text-white">
+                      <Li>Orderly, structured and legible</Li>
+                      <Li>Committed to code reusability and safety</Li>
+                      <Li>Architecturally-minded</Li>
+                    </List>
+                </div>
+                <div class="rounded-lg col-span-1 bg-center bg-cover bg-[url('src/resources/thethinker.jpg')]"></div>
             </div>
 
-            <div class="aspect-[16/9] col-start-1 col-end-3 lg:col-start-2 lg:col-end-5 xl:col-start-3 xl:col-end-9 bg-red-400">
-
+            <div class="rounded-lg aspect-[16/9] col-start-1 col-end-3 lg:col-start-2 lg:col-end-5 xl:col-start-3 xl:col-end-9 grid gap-2 p-2 lg:gap-4 grid-cols-2 bg-purple-600/20">
+                <div class="rounded-lg col-span-1 bg-center bg-cover bg-[url('src/resources/napoleoncrossing.jpg')]"></div>
+                <div class="col-span-1 p-2">
+                    <Heading tag="h2" class="font-sans text-xl lg:text-2xl xl:text-4xl font-bold text-white pb-4">Self-driven</Heading>
+                    <List tag="ul" class="space-y-1 font-sans text-base lg:text-lg xl:text-xl font-light text-white">
+                      <Li>Capable of self-teaching</Li>
+                      <Li>Confident and resilient</Li>
+                      <Li>Passionate, with a growth mentality</Li>
+                    </List>
+                </div>
             </div>
 
-            <div class="aspect-[16/9] col-start-1 col-end-3 lg:col-end-4 xl:col-end-7 bg-stone-500">
-
+            <div class="rounded-lg aspect-[16/9] col-start-1 col-end-3 lg:col-end-4 xl:col-end-7 grid gap-2 lg:gap-4 grid-cols-2 p-2 bg-fuchsia-900/20">
+                <div class="col-span-1 p-2">
+                    <Heading tag="h2" class="font-sans text-xl lg:text-2xl xl:text-4xl font-bold text-white pb-4">A deep thinker</Heading>
+                    <List tag="ul" class="space-y-1 font-sans text-base lg:text-lg xl:text-xl font-light text-white">
+                      <Li>Critical, logical, analytical</Li>
+                      <Li>Out-of-the-box solutions</Li>
+                      <Li>Creative and non-conformist</Li>
+                    </List>
+                </div>
+                <div class="rounded-lg col-span-1 bg-center bg-cover bg-[url('src/resources/nietzsche.jpg')]"></div>
             </div>
 
         </article>
